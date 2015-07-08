920ae458de9c540e7cdd546794fac347c1a1ae89
yumeka makishima <yumemeeee@gmail.com>
2015-07-08 09:48:41 +0900

    Pinterest  grid

--- summer/jquery.pinterestGrid.js
+++ summer/jquery.pinterestGrid.js
@@ -84,7 +84,7 @@
     function createEmptyGridArray() {
     //最初にgridArrayを初期化
         gridArray = [];
-        for (var i=0, i<numOfCol; i++){
+        for (var i=0; i<numOfCol; i++) {
             pushGridArray(i, 0, 1, -opts.offsetY);
         }
     }
@@ -137,7 +137,36 @@
         return heightArray;
     }
     
-    //gridが配置されるx座標値、y座標値を取得  
+    //gridが配置されるx座標値、y座標値を取得
+    var getGridPosition = function(size) {
+        if (size > 1) {
+            var arrayLimit = gridArray.length - size,
+                defined = false,
+                tempHeight;
+            for (var i=0; i<gridArray.length; i++) {
+                var obj = gridArray[i],
+                    x = obj.x;
+                if(x >= 0 && x <= arrayLimit) {
+                    var heightArray = getHeightArray(x, size);
+                    if(!defined) {
+                        defined = true;
+                        tempHeight = heightArray;
+                        tempHeight.x = x;
+                    }else {
+                        if (heightArray.max < tempHeight.max) {
+                            tempHeight = heightArray;
+                            tempHeight.x = x;
+                        }
+                    }
+                }
+            }
+            return tempHeight;
+        }else {
+            return getHeightArray(0, gridArray.length);
+        }
+    };
+    
+    /*
     function getGridPosition() {
         var pos = [];
         var tempHeight= getHeightArray(0, gridArray.length);
@@ -145,9 +174,47 @@
         pos.y = tempHeight.min;
         return pos;
     }
+    */
     
     //gridを配置 
-    function setPosition(grid){
+    var setPosition = function(grid) {
+        //check grid size
+        var size;
+        if(!grid.data('size') || grid.data('size') < 0){
+            size = 1;
+        }else if (grid.data('size') > numOfCol) { //extra
+            size = numOfCol;
+        }else {
+            size = grid.data('size');
+        }
+        //gridの情報を定義
+        var pos = [];
+        var tempHeight = getGridPosition(size); //extra
+        pos.x = tempHeight.x;
+        if (size > 1) {
+            pos.y = tempHeight.max;
+        }else {
+            pos.y = tempHeight.min;
+        }
+        var gridWidth = colWidth * size - (grid.outerWidth() - grid.width());  //extra
+        //gridのスタイルを更新
+        grid.css({
+            'width': gridWidth - opts.offsetX * 2, //extra
+            'left': pos.x * colWidth,
+            'top' : pos.y,
+            'position': 'absolute'
+        });
+        //gridArrayを新しいgridで更新
+        removeGridArray(pos.x, grid.data('size'));
+        pushGridArray(pos.x, pos.y, grid.data('size'), gridHeight);
+    };
+        
+})(jQuery);
+        
+        
+        
+        
+    /*function setPosition(grid){
         //check grid size
         if(!grid.data('size') || grid.data('size') < 0){
             grid.data('size',1);
